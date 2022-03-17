474732dcdaf903248bddbbf12a8aa1160bced0a3
spacedustt <97385811+spacedustt@users.noreply.github.com>
2022-03-17 22:21:52 +0100

    Update main navigation (Citizens launching soon + ME)

--- jollyranch/components/Navigation.tsx
+++ jollyranch/components/Navigation.tsx
@@ -4,13 +4,20 @@ import WalletMultiButtonStyled from "./shared/WalletMultiButtonStyled";
 const navigationItems = [
     {
         id: "shill-city-capital",
-        title: "Shill City Capital",
+        title: "Shanties",
         href: "https://staking.shill-city.com/"
     },
     {
         id: "pet-palace",
         title: "Pet Palace",
         href: "https://pets.shill-city.com/"
+    },
+    {
+        id: "shill-city-citizen",
+        title: "Citizens",
+        href: "",
+        tbr: true,
+        tbrMessage: "Citizen staking launching soon 🔱"
     },
     {
         id: "poseidon-lp",
@@ -41,14 +48,25 @@ const Navigation: FC<NavigationProps>  = ({activeId}) => {
                 </div>
             </div>
             <div className="flex sm:flex-grow md:basis-1/2 gap-3 md:gap-6 xl:gap-12 items-center md:flex-grow lg:place-content-center">
-                {navigationItems.map((item) => (
-                    <a
-                        key={item.id}
-                        href={item.href}
-                        className={`relative flex items-center h-full font-scratchy text-2xl md:text-4xl ${activeId === item.id ? 'text-yellow border-b-4 border-yellow' : 'text-secondary-content  hover:text-yellow'}`}>
-                        {item.title}
-                    </a>
-                ))}
+                {navigationItems.map((item) => {
+                    if(item.tbr) {
+                        return (
+                            <div className="tooltip cursor-default z-50 tooltip-bottom" data-tip={item.tbrMessage}>
+                                <div className="relative flex text-secondary-content/50 items-center h-full font-scratchy text-2xl md:text-4xl">
+                                    {item.title}
+                                </div>
+                            </div>
+                        )
+                    }
+                    return (
+                        <a
+                            key={item.id}
+                            href={item.href}
+                            className={`relative flex items-center h-full font-scratchy text-2xl md:text-4xl ${activeId === item.id ? 'text-yellow border-b-4 border-yellow' : 'text-secondary-content  hover:text-yellow'}`}>
+                            {item.title}
+                        </a>
+                    )
+                })}
             </div>
             <div className="lg:basis-1/4  items-center place-content-end pr-4 hidden sm:flex">
                 <a href="https://discord.com/invite/AA66Ayk5Dz" target="_blank" rel="noopener noreferrer">
@@ -56,6 +74,9 @@ const Navigation: FC<NavigationProps>  = ({activeId}) => {
                 </a>
                 <a href="https://twitter.com/SeaShantiesSol" target="_blank" rel="noopener noreferrer">
                     <img src="/images/twitter.svg" className="w-4 md:w-6 m-1 md:m-2" />
+                </a>
+                <a href="https://magiceden.io/creators/sea_shanties" target="_blank" rel="noopener noreferrer">
+                    <img src="/images/me.svg" className="w-4 md:w-7 m-1 md:m-2" />
                 </a>
                 <WalletMultiButtonStyled className="!btn-xs !w-[130px] ml-2 md:!btn-md md:!w-[170px]" />
             </div>
